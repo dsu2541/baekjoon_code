52724f36aa689e53e4fbf679e855448f2a828ba9
dsu2541 <128967868+dsu2541@users.noreply.github.com>
2025-06-08 10:23:34 +0900

    환경설정

--- src/Main.java
+++ src/Main.java
@@ -2,45 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //메인
         Scanner sc = new Scanner(System.in);
         int total = sc.nextInt();
-        int num = sc.nextInt();
 
-        int[] arr = new int[total+1];
-        for(int i=1;i<=total;i++){
-            arr[i] = i;
-        }
-
-        int order = 0;
-        int[] result_arr = new int[total+1];
-        int result_order = 0;
-        for(int i=1;i<=total;i++){
-            int cnt = num;
-            while(cnt>0){
-                order++;
-                if(order > total){
-                    order = 1;
-                }
-                if(arr[order] > 0){
-                    cnt--;
-
-                    if(cnt == 0){
-                        result_arr[result_order] = arr[order];
-                        arr[order] = -1;
-                        result_order++;
-                    }
-                }
-            }
-        }
-
-        System.out.print("<");
-        for(int i=0;i<=total-1;i++){
-            System.out.print(result_arr[i]);
-
-            if(i!=total-1){
-                System.out.print(", ");
-            }
-        }
-        System.out.print(">");
     }
 }
