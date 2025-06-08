4aabbfb040244e77cb8757566120893fec8a5082
dsu2541 <128967868+dsu2541@users.noreply.github.com>
2025-06-08 10:07:00 +0900

    초기세팅

---
+++ src/dynamic_programming/B_1463.java
@@ -0,0 +1,32 @@
+package dynamic_programming;
+
+import java.util.Scanner;
+
+public class B_1463 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+
+        int[] num = new int[input + 1];
+        num[1] = 0;
+
+        if(input > 1){
+            for (int i = 2; i <= input; i++) {
+                //case 1: 1을 뺀경우
+                num[i] = num[i-1] + 1;
+
+                //case 2: 2를 나눈 경우
+                if(i%2 == 0){
+                    num[i] = Math.min(num[i], num[i/2]+1);
+                }
+
+                //case 3: 3을 나눈 경우
+                if(i%3 == 0){
+                    num[i] = Math.min(num[i], num[i/3]+1);
+                }
+            }
+        }
+
+        System.out.print(num[input]);
+    }
+}
---
+++ src/stack_queue/B_11866.java
@@ -0,0 +1,51 @@
+package stack_queue;
+
+import java.util.Scanner;
+
+public class B_11866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        int num = sc.nextInt();
+
+        int[] arr = new int[total+1];
+        for(int i=1;i<=total;i++){
+            arr[i] = i;
+        }
+
+        int order = 0;
+        int[] result_arr = new int[total+1];
+        int result_order = 0;
+        for(int i=1;i<=total;i++){
+            int cnt = num;
+            while(cnt>0){
+                order++;
+                if(order > total){
+                    order = 1;
+                }
+                if(arr[order] > 0){
+                    cnt--;
+
+                    if(cnt == 0){
+                        result_arr[result_order] = arr[order];
+                        arr[order] = -1;
+                        result_order++;
+                    }
+                }
+            }
+        }
+
+        for(int i=0;i<=total-1;i++){
+            if(i==0){
+                System.out.print("<");
+            }
+            System.out.print(result_arr[i]);
+
+            if(i==total-1){
+                System.out.print(">");
+            }else{
+                System.out.print(", ");
+            }
+        }
+    }
+}
