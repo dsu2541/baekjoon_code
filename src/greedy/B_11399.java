1500f0cc97859f5b7815e548de1a302a134d3569
dsu2541 <128967868+dsu2541@users.noreply.github.com>
2025-06-10 18:27:37 +0900

    11399 ATM

--- src/greedy/B_11399.java
+++ src/greedy/B_11399.java
@@ -1,7 +1,9 @@
+package greedy;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class B_11399 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int cnt = sc.nextInt();
