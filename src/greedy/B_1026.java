a9528d50c10211bcb666d093439c8ba11e892dd2
dsu2541 <128967868+dsu2541@users.noreply.github.com>
2025-06-26 22:24:31 +0900

    1026 보물

--- src/greedy/B_1026.java
+++ src/greedy/B_1026.java
@@ -1,10 +1,12 @@
+package greedy;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class Main {
+public class B_1026 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
