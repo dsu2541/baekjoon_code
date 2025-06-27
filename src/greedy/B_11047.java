c661c9e4d440dad7d61add1b0514ad3dc708c3fa
dsu2541 <128967868+dsu2541@users.noreply.github.com>
2025-06-27 20:55:59 +0900

    11047 동전 0

--- src/greedy/B_11047.java
+++ src/greedy/B_11047.java
@@ -1,8 +1,10 @@
+package greedy;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class B_11047 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
