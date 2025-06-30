c13afa50a8476ec98a897648d0af00fb2d84c9ae
dsu2541 <128967868+dsu2541@users.noreply.github.com>
2025-06-30 20:57:13 +0900

    1158 요세푸스 문제

--- src/stack_queue/B_1158.java
+++ src/stack_queue/B_1158.java
@@ -1,10 +1,12 @@
+package stack_queue;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Main {
+public class B_1158 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
